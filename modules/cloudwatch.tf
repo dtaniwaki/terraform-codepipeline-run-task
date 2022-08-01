ec95aba4dcb6ad6134d2f5c659bb6c4abc069538
Daisuke Taniwaki <daisuketaniwaki@gmail.com>
2022-08-01 21:29:27 +0900

    Fix the log group name

--- modules/cloudwatch.tf
+++ modules/cloudwatch.tf
@@ -2,3 +2,8 @@ resource "aws_cloudwatch_log_group" "run_task" {
   name              = var.function_name
   retention_in_days = 30
 }
+
+resource "aws_cloudwatch_log_group" "run_task" {
+  name              = "/aws/lambda/${function_name}"
+  retention_in_days = 30
+}
